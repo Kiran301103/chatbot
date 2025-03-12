6a5d3b1a9a3a0e693381acea69fb0b9c86e54a8e
Kiran <124298040+Kiran301103@users.noreply.github.com>
2025-03-12 09:11:58 +0530

    Update gemini_utility.py

--- gemini_utility.py
+++ gemini_utility.py
@@ -19,13 +19,13 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 
 def load_gemini_pro_model():
-    gemini_pro_model = genai.GenerativeModel("gemini-pro")
+    gemini_pro_model = genai.GenerativeModel("gemini-1.5-pro-latest")
     return gemini_pro_model
 
 
 # get response from new Gemini model - image/text to text
 def gemini_pro_vision_response(prompt, image):
-    gemini_pro_vision_model = genai.GenerativeModel("gemini-1.5-flash")  # Updated model
+    gemini_pro_vision_model = genai.GenerativeModel("gemini-1.5-pro-latest")  # Updated model
     response = gemini_pro_vision_model.generate_content([prompt, image])
     result = response.text
     return result
@@ -43,7 +43,7 @@ def embeddings_model_response(input_text):
 
 # get response from Gemini-Pro model - text to text
 def gemini_pro_response(user_prompt):
-    gemini_pro_model = genai.GenerativeModel("gemini-pro")
+    gemini_pro_model = genai.GenerativeModel("gemini-1.5-pro-latest")
     response = gemini_pro_model.generate_content(user_prompt)
     result = response.text
     return result
